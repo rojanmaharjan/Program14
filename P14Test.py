fe96bc4ed5e37158cde1a482707498023944881f
Rojan Maharjan <qn8676ik@mnstate.edu>
2016-04-27 12:46:26 -0500

    added constructor

--- P14Test.py
+++ P14Test.py
@@ -2,3 +2,25 @@
 Program 14 - Set Module Test File
 Members: Kyle, Henok, Rojan
 '''
+
+
+from modSet import *
+
+
+
+set1 = CustomSet([1,2,3,4,5])
+set2 = CustomSet([3,4,5,6])
+set3 = CustomSet([1,1,1,1,])
+
+print("Custome Set: " , set3)
+print("\n")
+
+
+union = set1.unionSet(set2)
+
+print("Union Set: ",union)
+print("\n")
+
+intersection = set2.interSectionSet(set1)
+
+print("Intersection Set: ",intersection)
--- modSet.py
+++ modSet.py
@@ -2,3 +2,91 @@
 Program 14 - Set Module
 Members: Kyle, Henok, Rojan
 '''
+
+class CustomSet:
+    """
+    Description: This function creates a class Called CustomSet
+    Pre-Condition:None 
+    Post-Condition:None
+    """
+
+    def __init__(self, tmpSetList = ["None"]):
+        """
+        Description: creates a CustomSet Object
+        Pre-Condition: tmpSetList is type set
+        Post-Condtion: updates the value of attributes
+        """
+
+        newList = []
+        for ch in tmpSetList:
+            if ch not in newList:
+                newList.append(ch)
+        
+        self._setList = newList
+
+        
+
+
+
+
+    def getSetList(self):
+        """
+        Description: retrives self._setList
+        Pre-Condition: None
+        Post-Condtion: None
+        """
+
+        return self._setList
+
+
+
+    def unionSet(self, newSet):
+        """
+        Description: creates a union set by adding two different set
+        Pre-Condition: newSet is type set
+        Post-Condtion: returns new object union which is type list
+        """
+
+        union = []
+        for ch in self._setList:
+            union.append(ch)
+        for ch in newSet.getSetList():
+            if ch not in self._setList:
+                union.append(ch)
+
+        return union
+
+        
+    
+     
+
+        
+
+    def interSectionSet(self, newSet):
+        """
+        Description: creates a intersection set by adding only common character
+                     from two different set
+        Pre-Condition: newSet is type list
+        Post-Condtion: returns new object intersection which is type list
+        """
+
+        intersection = []
+
+        for num in self._setList:
+            if num in newSet.getSetList():
+                intersection.append(num)
+        return intersection
+
+
+
+    def __str__(self):
+        """
+        Description: returns all of the value of attributes
+        Pre-Condition:
+        Post-Condition:
+        """
+
+        tmp = ""
+        tmp += str(self._setList)
+        return tmp
+        
